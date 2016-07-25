fbc527d1b37f701d8d704acae7f54431d360a671
Afkham Azeez <azeez@wso2.com>
2016-07-25 18:36:09 +0530

    Initial commit

---
+++ src/main/java/org/example/service/LogHeadersInterceptor.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.wso2.msf4j.Interceptor;
+import org.wso2.msf4j.Request;
+import org.wso2.msf4j.Response;
+import org.wso2.msf4j.ServiceMethodInfo;
+
+import java.util.Map;
+
+/**
+ * Spring based MSF4J Interceptor class which print HTTP headers from incoming messages.
+ */
+@Component
+public class LogHeadersInterceptor implements Interceptor {
+
+    @Autowired
+    private Util util;
+
+    @Override
+    public boolean preCall(Request request, Response responder, ServiceMethodInfo serviceMethodInfo) throws Exception {
+        Map<String, String> headers = request.getHeaders();
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            util.print("Header - " + entry.getKey() + " : " + entry.getValue());
+        }
+        return true;
+    }
+
+    @Override
+    public void postCall(Request request, int status, ServiceMethodInfo serviceMethodInfo) throws Exception {
+
+    }
+}
