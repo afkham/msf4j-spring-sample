c14df38cb6e05de98b04f40fbf7b77c8c18fe4d0
Afkham Azeez <azeez@wso2.com>
2016-09-29 17:01:25 +0530

    Added @Produces annotation

--- src/main/java/org/example/service/ProductService.java
+++ src/main/java/org/example/service/ProductService.java
@@ -29,6 +29,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
@@ -46,6 +48,7 @@ public class ProductService {
 
     @GET
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response get(@PathParam("id") int id) {
         util.print("Getting product with ID " + id);
         Product product = productUtil.find(id);
